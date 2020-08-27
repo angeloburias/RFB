d5802dc5166365965f552faede92d4dd41c976fa
angelo burias <angeloburias8@gmail.com>
2020-08-27 14:54:58 -0700

    Revert "Revert "added new files""
    This reverts commit 8a7aa5d2a88da5460bae8c037895966a2c726f46.

---
+++ StaticRFBAirconditioningServices/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaticRFB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About us.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact us.";
+
+            return View();
+        }
+
+        public ActionResult team()
+        {
+            ViewBag.Message = "Dev Team";
+
+            return View();
+        }
+
+
+
+        public ActionResult services()
+        {
+            ViewBag.Message = "Services";
+
+            return View();
+        }
+
+        public ActionResult Cleaning()
+        {
+            ViewBag.Message = "General Cleaning";
+
+            return View();
+        }
+
+
+        public ActionResult Checkup()
+        {
+            ViewBag.Message = "General Cleaning";
+
+            return View();
+        }
+
+
+        public ActionResult Repair()
+        {
+            ViewBag.Message = "General Cleaning";
+
+            return View();
+        }
+    }
+}
